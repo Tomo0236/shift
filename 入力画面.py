cd33a0c8abaacbb12dceccd8d7dce30e0d31412c
Tomo0236 <s2071092@s.konan-u.ac.lp>
2023-11-13 23:33:23 +0900

    ファイル

--- 入力画面.py
+++ 入力画面.py
@@ -10,19 +10,8 @@ st.session_state.book = load_workbook('卒業研究_シフトデータ.xlsx')
 sh1 = st.session_state.book['入力データ']
 sh2 = st.session_state.book['シフト表']
 
-#初期情報の入力
 st.title('ライブ裏方シフトのシフト表')
 st.header('▼ 初期情報の入力')
-
-member = st.number_input('人数を決めてください', min_value=0, max_value=100)
-st.write('→ 人数は', member, '人です。')
-slot = st.number_input('出演バンド数を決めてください', min_value=0, max_value=100)
-st.write('→ 出演バンドは', slot, 'バンドです。')
-
-#定数用のデータの作成
-st.session_state.I = [i+1 for i in range(member)]
-st.session_state.J = [i+1 for i in range(4)]
-st.session_state.T = [i+1 for i in range(slot)]
 
 @st.cache_resource
 def input_list_member():
@@ -30,44 +19,69 @@ def input_list_member():
     return st.session_state.lst_member
 
 @st.cache_resource
-def input_list_turn():
-    lst_turn = []
-    return lst_turn
+def input_dict_turn_member():
+    dict_turn_member = {}
+    return dict_turn_member
+
+#コールバック関数
+def clear_member():
+    input_list_member.clear()
+    st.session_state.lst_member = input_list_member()
 
 #メンバーの名前入力
+st.session_state.lst_member = input_list_member()
+dict_turn_member = input_dict_turn_member()
+with st.form('input_name', clear_on_submit=True):
+    st.write('メンバーの名前を入力してください')
+    member_name = st.text_input('↓↓↓')
+    submitted = st.form_submit_button('メンバー追加')
+    if submitted:
+        st.session_state.lst_member.append(member_name)
+        dict_turn_member[member_name] = 0
+    st.table(st.session_state.lst_member)
+member = len(st.session_state.lst_member)
+btn = st.button('clear member', on_click=clear_member)
+
+#バンド数入力
+slot = st.number_input('出演バンド数を決めてください', min_value=1, max_value=100)
+st.write('→ 出演バンドは', slot, 'バンドです。')
+    
+#定数用のデータの作成
+st.session_state.I = [i+1 for i in range(member)]
+st.session_state.J = [i+1 for i in range(4)]
+st.session_state.T = [i+1 for i in range(slot)]
+
 if int(member) > 0:
-    st.session_state.lst_member = input_list_member()
-    lst_turn = input_list_turn()
-    with st.form('input_name', clear_on_submit=True):
-        st.write('メンバーの名前を入力してください')
-        member_name = st.text_input('↓↓↓')
-        submitted = st.form_submit_button('メンバー追加')
-        if submitted:
-            st.session_state.lst_member.append(member_name)
-            lst_turn.append(0)
-        st.table(st.session_state.lst_member)
-    if st.button('clear member'):
-        input_list_member.clear()
-        st.session_state.lst_member = input_list_member()
         
-    if len(lst_turn) > 0:
-        with st.form('input_turn', clear_on_submit=False):
-            if lst_turn.count(0) > 0:
-                st.write(st.session_state.lst_member[lst_turn.index(0)], 'が何バンド目に出演するか入力してください')
-            elif lst_turn.count(0) == 0:
+#コールバック関数
+    def update_index():
+        st.session_state['num'] += 1
+
+#コールバック関数
+    def clear_turn():
+        input_dict_turn_member.clear()
+        dict_turn_member = input_dict_turn_member()
+        st.session_state['num']= 0
+
+#出演バンドの入力
+    if len(dict_turn_member) > 0:
+        with st.form('input_turn', clear_on_submit=True):
+            if 'num' not in st.session_state:
+                st.session_state['num'] = 0
+            if st.session_state['num'] < int(member):
+                st.write(st.session_state.lst_member[st.session_state['num']], 'が何バンド目に出演するか入力してください')
+            if st.session_state['num'] == int(member):
                 st.write('入力完了')
+                lst_turn = list(dict_turn_member.values())
             member_turn = st.number_input('↓↓↓', min_value=1, max_value=slot)
-            submitted = st.form_submit_button('出演情報追加')
+            submitted = st.form_submit_button('出演情報追加', on_click=update_index)
             if submitted:
-                lst_turn[lst_turn.index(0)] = member_turn
-            st.table(lst_turn)
-        if st.button('clear turn'):
-            input_list_turn.clear()
-            lst_turn = input_list_turn()
-    
-    
-#Excel書き込み
-    if len(lst_turn) > 0:
+                dict_turn_member[st.session_state.lst_member[st.session_state['num']-1]] = member_turn
+            st.table(dict_turn_member)
+        btn = st.button('clear turn', on_click=clear_turn)
+
+    #Excel書き込み
+    if len(dict_turn_member) > 0:
         st.header('▼ 入力データ保存')
         if st.button('データ保存'):
             status = st.empty()
